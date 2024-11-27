56445ae29a5abb2ef0ad6182bb0efbc92bc2d35a
olhapatsyienko <20107984@mail.wit.ie>
2024-11-27 16:45:27 +0000

    Drinks class was updated with URL feature; Ingredient and drink views were created.

--- src/main/java/com/example/drink_system/Drinks.java
+++ src/main/java/com/example/drink_system/Drinks.java
@@ -5,10 +5,13 @@ public class Drinks {
     private String countryOfOrigin;
     private String textualDescription;
 
-    public Drinks(String drinkName, String countryOfOrigin, String textualDescription) {
+    private String imageURL; //Peter asks for URL in the description and not an actual pic so I hope we are gonna be fine with that
+
+    public Drinks(String drinkName, String countryOfOrigin, String textualDescription, String imageURL) {
         this.drinkName = drinkName;
         this.countryOfOrigin = countryOfOrigin;
         this.textualDescription = textualDescription;
+        this.imageURL = imageURL;
     }
 
     public String getDrinkName() {
@@ -35,8 +38,14 @@ public class Drinks {
         this.textualDescription = textualDescription;
     }
 
+    public String getImageURL(){
+        return imageURL;
+    }
+    public void setImageURL(){
+        this.imageURL = imageURL;
+    }
     public String toString() {
-        return "Drink Name: " + drinkName + ", Country of Origin: " + countryOfOrigin + " Textual Description: " + textualDescription;
+        return "Drink Name: " + drinkName + ", Country of Origin: " + countryOfOrigin + ", Textual Description: " + textualDescription + ", Image URL: " +imageURL;
     }
 
 }
--- src/main/java/com/example/drink_system/HelloController.java
+++ src/main/java/com/example/drink_system/HelloController.java
@@ -1,7 +1,10 @@
 package com.example.drink_system;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
@@ -11,4 +14,35 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    //ingredients tab
+    @FXML
+    private TextField ingredientNameField;
+    @FXML
+    private TextField ingredientDescriptionField;
+    @FXML
+    private TextField ingredientAlcContentField;
+    @FXML
+    private Button ingredientAddButton;
+    @FXML
+    private Button ingredientDeleteButton;
+    @FXML
+    private ListView ingredientListView;
+
+    //Drinks Page
+    @FXML
+    private TextField drinkNameField;
+    @FXML
+    private TextField drinkDescriptionField;
+    @FXML
+    private TextField drinkCountryOfOriginField;
+    @FXML
+    private TextField drinkURLImageField;
+    @FXML
+    private Button drinkAddButton;
+    @FXML
+    private Button drinkDeleteButton;
+    @FXML
+    private ListView drinkListView;
+
 }
