09cca600953d618130565d11689a00e21bd73105
olhapatsyienko <20107984@mail.wit.ie>
2024-11-27 17:44:47 +0000

    Add method was added

--- src/main/java/com/example/drink_system/HelloController.java
+++ src/main/java/com/example/drink_system/HelloController.java
@@ -1,11 +1,15 @@
 package com.example.drink_system;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.ObjectOutputStream;
 public class HelloController {
     @FXML
     private Label welcomeText;
@@ -29,6 +33,30 @@ public class HelloController {
     @FXML
     private ListView ingredientListView;
 
+    private CustomLinkedList<Ingredients> ingredientsCustomLinkedList = new CustomLinkedList<>();
+    public void addIngredient (ActionEvent event){
+        String name = ingredientNameField.getText();
+        String description = ingredientDescriptionField.getText();
+        double alcContent = Double.parseDouble(ingredientAlcContentField.getText());
+
+        Ingredients newIngredient = new Ingredients(name, description, alcContent);
+
+        ingredientsCustomLinkedList.add(newIngredient);
+        ingredientListView.getItems().add(newIngredient.toString());
+
+        ingredientNameField.clear();
+        ingredientDescriptionField.clear();
+        ingredientAlcContentField.clear();
+    }
+
+
+    public void deleteIngredient(ActionEvent event) {
+        //I am not sure how you do the delete method
+    }
+
+
+
+
     //Drinks Page
     @FXML
     private TextField drinkNameField;
@@ -37,12 +65,32 @@ public class HelloController {
     @FXML
     private TextField drinkCountryOfOriginField;
     @FXML
-    private TextField drinkURLImageField;
+    private TextField drinkURLImageField; //we will need to implement hyperlink feature
     @FXML
     private Button drinkAddButton;
     @FXML
     private Button drinkDeleteButton;
     @FXML
     private ListView drinkListView;
+    private CustomLinkedList<Drinks> drinksCustomLinkedList = new CustomLinkedList<>();
+    public void addDrink (ActionEvent event) {
+        String name = drinkNameField.getText();
+        String description = drinkDescriptionField.getText();
+        String origin = drinkCountryOfOriginField.getText();
+        String url = drinkURLImageField.getText();
+
+        Drinks newDrink = new Drinks(name, description, origin, url);
+
+        drinksCustomLinkedList.add(newDrink);
+        drinkListView.getItems().add(newDrink.toString());
+
+        drinkNameField.clear();
+        drinkDescriptionField.clear();
+        drinkCountryOfOriginField.clear();
+        drinkURLImageField.clear();
+    }
+    public void deleteDrink(ActionEvent event){
+
+    }
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module com.example.drink_system {
 
     opens com.example.drink_system to javafx.fxml;
     exports com.example.drink_system;
+
+
 }
